717be41b84d810b5be824a9af02f68ba1c4fe2f4
devendra-bhatt-a10 <git@github.com:a10networks/terraform-provider-vthunder-automation.git>
2020-04-29 11:13:34 +0530

    Added logging.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM ubuntu:latest
-RUN apt-get update -y
+RUN apt-get update
+RUN apt-get upgrade -y
 RUN apt-get install -y python3-pip python3 build-essential
 COPY . /app
 WORKDIR /app
--- acos_exporter.py
+++ acos_exporter.py
@@ -32,8 +32,13 @@ def get_valid_token(host_ip, to_call=False):
         if host_ip in tokens and not to_call:
             return tokens[host_ip]
         else:
+            token = ""
             if host_ip not in tokens or to_call:
-                tokens[host_ip] = getauth(host_ip)
+                token = getauth(host_ip)
+            if not token:
+                logger.error("Token not received.")
+                return ""
+            tokens[host_ip] = token
         return tokens[host_ip]
     finally:
         lock1.release()
@@ -81,9 +86,13 @@ def getauth(host):
             logger.error("password not provided.")
 
         payload = {'Credentials': {'username': uname, 'password': pwd}}
-        auth = json.loads(
-            requests.post("https://{host}/axapi/v3/auth".format(host=host), json=payload, verify=False).content.decode(
-                'UTF-8'))
+        try:
+            auth = json.loads(requests.post("https://{host}/axapi/v3/auth".format(host=host), json=payload,
+                                            verify=False, timeout=5).content.decode('UTF-8'))
+        except requests.exceptions.Timeout:
+            logger.error("Connection to {host} timed out. (connect timeout=5 secs)".format(host=host))
+            return ''
+
         if 'authresponse' not in auth:
             logger.error("Host credentials are not correct")
             return ''
@@ -92,20 +101,34 @@ def getauth(host):
 
 @app.route("/")
 def default():
-    return "Please provide /metrics/service-name!"
+    return "Please provide /metrics?query-params!"
 
 
 @app.route("/metrics")
 def generic_exporter():
+    logger.debug("----------------------------------------------------------------------------------------------------")
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    host_ip = request.args["host_ip"]
-    api_endpoint = request.args["api_endpoint"]
-    api_name = request.args["api_name"]
+    host_ip = request.args.get("host_ip","")
+    api_endpoint = request.args.get("api_endpoint", "")
+    api_name = request.args.get("api_name", "")
+    # partition = request.args.get("partition", "")
+
+    res = []
+    if not api_endpoint:
+        logger.error("api_endpoint is required.")
+        return Response(res, mimetype="text/plain")
+    if not host_ip:
+        logger.error("host_ip is required. Exiting API endpoint - ", api_endpoint)
+        return Response(res, mimetype="text/plain")
+    if not api_name:
+        logger.error("api_name is required.")    # return empty
+        return Response(res, mimetype="text/plain")
+
     token = get_valid_token(host_ip)
 
     logger.info("Host = " + host_ip + "\t" +
-                "API = " + api_name + "\t\t" +
-                "Endpoint = " + api_endpoint)
+                "API = " + api_name)
+    logger.info("Endpoint = " + api_endpoint)
 
     endpoint = "https://{host_ip}/axapi/v3".format(host_ip=host_ip)
     headers = {'content-type': 'application/json', 'Authorization': token}
@@ -113,6 +136,7 @@ def generic_exporter():
 
     response = json.loads(
         requests.get(endpoint + api_endpoint + "/stats", headers=headers, verify=False).content.decode('UTF-8'))
+    logger.debug("AXAPI response - ", response)
 
     if 'response' in response and 'err' in response['response']:
         msg = response['response']['err']['msg']
@@ -121,18 +145,22 @@ def generic_exporter():
 
         elif str(msg).lower().__contains__("unauthorized"):
             token = get_valid_token(host_ip, True)
-            headers = {'content-type': 'application/json', 'Authorization': token}
-            logger.info("Re-executing an api -" + endpoint + api_endpoint + "with the new token")
-            response = json.loads(
-                requests.get(endpoint + api_endpoint + "/stats", headers=headers, verify=False).content.decode('UTF-8'))
+            if token:
+                logger.info("Re-executing an api -", endpoint, api_endpoint + "/stats", " with the new token")
+                headers = {'content-type': 'application/json', 'Authorization': token}
+                response = json.loads(
+                    requests.get(endpoint + api_endpoint + "/stats", headers=headers, verify=False).content.decode('UTF-8'))
+        else:
+            logger.error("Unknown error message - ", msg)
 
     try:
         key = list(response.keys())[0]
         event = response.get(key)
+        logger.debug("event - " + str(event))
         stats = event.get("stats", {})
     except Exception as ex:
         logger.exception(ex)
-        return api_endpoint + " have something missing."
+        return api_endpoint + " has something missing."
 
     api = str(api_name)
     if api.startswith("_"):
@@ -160,9 +188,10 @@ def generic_exporter():
             global_stats[key].labels(api).set(stats[org_key])
 
     global_api_collection[api] = current_api_stats
-    res = []
+
     for name in global_api_collection[api]:
         res.append(prometheus_client.generate_latest(global_api_collection[api][name]))
+    logger.debug("Final Response - " + str(res))
     return Response(res, mimetype="text/plain")
 
 
@@ -174,7 +203,7 @@ if __name__ == '__main__':
     try:
         with open('config.json') as f:
             log_data = json.load(f).get("log", {})
-            logger = set_logger(log_data.get("log_file","logs.log"), log_data.get("log_level","DEBUG"))
+            logger = set_logger(log_data.get("log_file","logs.log"), log_data.get("log_level","INFO"))
             logger.info("Starting exporter")
             main()
     except Exception as e:
